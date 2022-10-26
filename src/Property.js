de75c43cd817027db1e34c779f282234f3bd16d2
Samir Kaushik <samirkaushik2002@gmail.com>
2022-10-26 08:35:46 +0530

    added links to locations

--- src/Property.js
+++ src/Property.js
@@ -1,7 +1,8 @@
 import React from "react";
 const Property = ({ data, key }) => {
     return (
-        <div className="property">
+        
+        <div className="property"><a href={data.url} target="_blank" style={{textDecoration:'none'}}>
             <div className="image"><img src={data.imageUrls[0]} alt={data.title} /></div>
             <div className="description">
                 <div className="price">
@@ -12,8 +13,9 @@ const Property = ({ data, key }) => {
                 <div className="address">{data.address}</div>
                 <div className="greybar"></div>
                 <div className="closed">{data.temporarilyClosed ? <span style={{ color: 'red', borderRadius: '0 0 10px 10px' }}>Closed</span> : <span style={{ color: 'green', borderRadius: '0 0 10px 10px' }}>Open</span>}</div>
-            </div>
+            </div></a>
         </div>
+        
     );
 }
 export default Property;
